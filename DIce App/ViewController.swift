320580c1649215d9289f8324a50f8799725e1161
Dilip Rao <DRao@Dilips-MacBook-Air.local>
2016-12-08 11:12:47 -0600

    Initial Commit

---
+++ DIce App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DIce App
+//
+//  Created by Dilip Rao on 12/8/16.
+//  Copyright © 2016 Dilip Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
